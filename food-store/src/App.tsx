760a5a7352518ea0b41cb58ccbaf9638eb48cc70
Rafaaxx <rafa.navarro.2006@gmail.com>
2026-05-01 13:23:10 -0300

    crear menuscreen

--- food-store/src/App.tsx
+++ food-store/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import LoginScreen from './pages/LoginScreen'
+import MenuScreen from './pages/MenuScreen'
 
 function App() {
 
   return (
    <Routes>
     <Route path='/login' element={<LoginScreen/>}/>
-   </Routes>
+    <Route path='/menu' element={<MenuScreen/>}/>   
+    </Routes>
   )
 }
 
---
+++ food-store/src/pages/MenuScreen.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function MenuScreen() {
+  return (
+    <>
+    <div className="flex flex-col items-center mt-10 justify-center">
+        <h1 className="font-bold text-7xl mb-10">Bienvenido</h1>
+        <div>
+        <Link to="/ingredientes" className="border rounded-full bg-blue-600 text-white font-bold p-3 m-2">Ver todos los ingredientes</Link>
+        <Link to="/productos" className="border rounded-full bg-blue-600 text-white font-bold p-3 m-2">Ver todos los productos</Link>
+        <Link to="/categorias" className="border rounded-full bg-blue-600 text-white font-bold p-3 m-2">Ver todas las categorias</Link>
+        </div>
+    </div>
+    </>
+  )
+}
