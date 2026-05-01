f8822ad32039632e11966dd25da35ca66f94e2ce
Rafaaxx <rafa.navarro.2006@gmail.com>
2026-05-01 17:39:48 -0300

    agregar contexts y mainprovider

--- food-store/src/main.tsx
+++ food-store/src/main.tsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
+import { MainProvider } from './context/MainProvider.tsx'
 
 // Marcamos la URL base para las solicitudes HTTP
 axios.defaults.baseURL = 'http://localhost:3000'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <MainProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </MainProvider>
   </StrictMode>,
 )
