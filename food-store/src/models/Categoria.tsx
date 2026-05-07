17c8c7d4ad7a9c4dbf2ed99641462a992df1467c
lautif005 <lautiferreria@gmail.com>
2026-05-07 17:10:31 -0300

    context adaptado a backend

--- food-store/src/models/Categoria.tsx
+++
@@ -1,9 +0,0 @@
-export interface Categoria {
-    id: number;
-    nombre: string;
-    descripcion: string;
-    parent_id?: number
-    imagen_url: string;
-    activo: boolean;
-    hijos?: number[]
-}
