778d34428f2839a93046eb8f88293b930954badb
Nikolan <nicolassantiagonavarrete.nsn@gmail.com>
2026-05-07 22:32:03 -0300

    Rutas Protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation, useNavigate} from 'react-router-dom'
 import LoginScreen from './pages/LoginScreen'
 import MenuScreen from './pages/MenuScreen'
 import ListaIngredientesScreen from './pages/ListaIngredientesScreen'
@@ -7,18 +7,30 @@ import CrearIngredienteScreen from './pages/CrearIngredienteScreen'
 import EditarIngredienteScreen from './pages/EditarIngredienteScreen'
 import { ProductosPage } from './pages/ProductosPage'
 import CategoriaScreen from './pages/CategoriaScreen'
+import ProtectedRoute from './features/ProtectedRoute'
+import { useEffect } from 'react'
 
 function App() {
 
+  const location = useLocation()
+  const navigate = useNavigate()
+
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/login')
+    }
+  }, [])
+
   return (
-   <Routes>
-    <Route path='/login' element={<LoginScreen/>}/>
-    <Route path='/menu' element={<MenuScreen/>}/>
-    <Route path='/ingredientes' element={<ListaIngredientesScreen/>}/>
-    <Route path='/formulario-ingrediente' element={<CrearIngredienteScreen/>}/>
-    <Route path='/ingredientes/editar/:id' element={<EditarIngredienteScreen/>}/>
-    <Route path='/productos' element={<ProductosPage/>} />
-    <Route path='/categorias' element={<CategoriaScreen/>}/>
+  <Routes>
+  <Route path='/login' element={<LoginScreen/>}/>
+  <Route path='/menu' element={<ProtectedRoute><MenuScreen/></ProtectedRoute>} />
+  <Route path='/ingredientes' element={<ProtectedRoute><ListaIngredientesScreen/></ProtectedRoute>} />
+  <Route path='/formulario-ingrediente' element={<ProtectedRoute><CrearIngredienteScreen/></ProtectedRoute>} />
+  <Route path='/ingredientes/editar/:id' element={<ProtectedRoute><EditarIngredienteScreen/></ProtectedRoute>} />
+  <Route path='/productos' element={<ProtectedRoute><ProductosPage/></ProtectedRoute>} />
+  <Route path='/categorias' element={<ProtectedRoute><CategoriaScreen/></ProtectedRoute>} />
    </Routes>
   )
 }
