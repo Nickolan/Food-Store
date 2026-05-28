6535696d5c5fa7b2e91b48504e2956d107b7997e
Lucas <lucas.gordillo.3591@gmail.com>
2026-05-28 13:18:41 -0300

    navbar terminado

--- src/features/LoginForm.tsx
+++ src/features/LoginForm.tsx
@@ -10,7 +10,20 @@ const LoginForm = () => {
         defaultValues: { email: "", password: "" },
         onSubmit: async ({ value }) => {
             const ok = await auth?.login(value.email, value.password)
-            if (ok) navigate('/')
+            if (ok){
+                console.log("Login exitoso, redirigiendo a dashboard...");
+                if (auth?.getUsuarioFromToken) {
+                    try {
+                        await auth.getUsuarioFromToken()
+                        console.log(" Usuario cargado correctamente")
+                    } catch (error) {
+                        console.error("Error cargando usuario:", error)
+                    }
+                } else {
+                    console.warn(" getUsuarioFromToken no disponible en auth context")
+                }
+
+            } navigate('/')
         }
     })
 
--- src/features/Sidebar.tsx
+++ src/features/Sidebar.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
       </div>
       <nav className="flex flex-col gap-2 mt-2">
         <NavLink
-          to="/categorias"
+          to="categorias"
           className={({ isActive }) =>
             `block transition-colors ${
               isActive
@@ -22,7 +22,7 @@ export default function Sidebar() {
           Categorías
         </NavLink>
         <NavLink
-          to="/ingredientes"
+          to="ingredientes"
           className={({ isActive }) =>
             `block transition-colors ${
               isActive
@@ -34,7 +34,7 @@ export default function Sidebar() {
           Ingredientes
         </NavLink>
         <NavLink
-          to="/productos"
+          to="productos"
           className={({ isActive }) =>
             `block transition-colors ${
               isActive
